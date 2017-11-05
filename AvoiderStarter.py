debea38b26b6c302e2dcef4c9dfd450ca7568b5d
Zach Mills <33356985+ZachMills@users.noreply.github.com>
2017-11-04 22:25:01 -0600

    Update AvoiderStarter.py
    Fixed errors and added two wagon cycles.
    -fixed hero movement
    -Variable wagons

--- AvoiderStarter.py
+++ AvoiderStarter.py
@@ -1,4 +1,4 @@
-# Starter code for an avoider game.
+## Starter code for an avoider game.
 # University of Utah, David Johnson, 2017.
 # This code, or code derived from this code, may not be shared without permission.
 
@@ -6,11 +6,11 @@ import sys, pygame, math
 
 # This function loads a series of sprite images stored in a folder with a
 # consistent naming pattern: sprite_# or sprite_##. It returns a list of the images.
-def load_piskell_sprite(sprite_folder_name, number_of_frames):
+def load_piskell_sprite(sprite_folder_name, sprite_name ,number_of_frames):
     frame_counts = []
     padding = math.ceil(math.log(number_of_frames-1,10))
     for frame in range(number_of_frames):
-        folder_and_file_name = sprite_folder_name + "/sprite_" + str(frame).rjust(padding,'0') +".png"
+        folder_and_file_name = sprite_folder_name + "/" + sprite_name + str(frame) + ".png"
         frame_counts.append(pygame.image.load(folder_and_file_name).convert_alpha())
                              
     return frame_counts
@@ -39,17 +39,18 @@ def main():
     myfont = pygame.font.SysFont("monospace", 24)
 
     # Load in the background image
-    map = pygame.image.load("BG.png")
+    world = pygame.image.load("images/BG.png")
+    #Big_rect = world.get_rect()
 
     # Store window width and height in different forms for easy access
-    map_size = map.get_size()
+    world_size = world.get_size()
 
     # The map rect is basically the whole screen, and we will draw to it to fill the background with the image
-    map_rect = map.get_rect()
+    world_rect = world.get_rect()
     
     # create the window the same size as the map image
-    screen = pygame.display.set_mode(map_size)
-        
+    screen = pygame.display.set_mode(world_size)
+    
     # The frame_count counts all the frames that have passed since the start of the game.
     # Look at the print statements in the loop to see how to use the count with a mod function
     # to get cycles of different lengths.
@@ -64,14 +65,23 @@ def main():
     # is_alive means that the game loop should continue. Winning or losing the game sets is_alive to False.
     is_alive = True
 
+    map_tile_width = 30
+    map_tile_height = 20
+    tile_size = 32
+    screen_size = width, height = (map_tile_width*tile_size, map_tile_height*tile_size)
     #Hero: Load Hero/Getting Rect.
-    hero = load_piskell_sprite("Hero",4)
+    hero = load_piskell_sprite("images/Hero", "sprite_",4)
     hero_rect = hero[0].get_rect()
-    print(hero)
+    hero_rect.center = (width/2, height/2)
+    #print(hero)
     #Enemy 1: Load Enemy/Getting Rect.
-    enemy1 = load_piskell_sprite("Wagon",4)
+    enemy1 = load_piskell_sprite("images/Wagon","Wagon",4)
     enemy1_rect = enemy1[0].get_rect()
-    print(enemy1)
+    enemy1_rect.center = (0, height/2)
+    #print(enemy1)
+    enemy2 = load_piskell_sprite("images/Wagon","Wagon",4)
+    enemy2_rect = enemy2[0].get_rect()
+    enemy2_rect.center = (0, (height/2)-20)
 
     #Speed Of Character: Higher the number faster it goes per pixel/square
     speed = 5
@@ -96,21 +106,31 @@ def main():
                     hero_rect.move_ip(-speed,0)
                     frame_number += 1
                 if event.key == pygame.K_RIGHT:
-                    hero_rect.move_ip(0,speed)
+                    hero_rect.move_ip(speed,0)
                     frame_number += 1
                 if event.key == pygame.K_UP:
-                    hero_rect.move_ip(0,speed)
+                    hero_rect.move_ip(0,-speed)
                     frame_number += 1
                 if event.key == pygame.K_DOWN:
-                    hero_rect.move_ip(0,-speed)
+                    hero_rect.move_ip(0,speed)
                     frame_number += 1
             if frame_number >= len(hero):
                 frame_number = 0
-            
-                    
 
-        screen.blit(map, map_rect, hero, hero_rect)
+        enemy1_rect.move_ip(speed,0)
+        if (enemy1_rect.x > width):
+            enemy1_rect.center = (0, height/2)
+        
+        enemy2_rect.move_ip(speed*2,0)
+        if (enemy2_rect.x > width):
+            enemy2_rect.center = (0, (height/2)-10)
+        
 
+        
+        screen.blit(world, world_rect)
+        screen.blit(hero[frame_number%len(hero)], hero_rect)
+        screen.blit(enemy1[frame_number%len(enemy1)], enemy1_rect)
+        screen.blit(enemy2[frame_number%len(enemy2)], enemy2_rect)
 
 
 #This Code uses a mouse as the character
@@ -134,8 +154,8 @@ def main():
         # You may have sprites with different numbers of frames. We can make cycles
         # of different lengths by using mod on the frame_count. This is easier than
         # maintaining a different frame count variable for each different sprite.
-        print("Cycle of length 3:", frame_count%3) # counts 0,1,2,0,1,2
-        print("Cycle of length 4:", frame_count%4) # counts 0,1,2,3,0,1,2,3
+        #print("Cycle of length 3:", frame_count%3) # counts 0,1,2,0,1,2
+        #print("Cycle of length 4:", frame_count%4) # counts 0,1,2,3,0,1,2,3
 
         # Render text to the screen
         label = myfont.render("By: Isabella and Zach", True, (255,255,0))
@@ -149,7 +169,7 @@ def main():
 
         # This tries to force the loop to run at 1/2 fps. The is artifically slow so the output above
         # can be inspected. You should change this speed. Something like 30 is more normal.
-        clock.tick(0.5)
+        clock.tick(60)
 
     # This happens once the loop is finished - the game is over.
     pygame.quit()
